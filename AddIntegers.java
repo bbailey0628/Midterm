3e7f41ac5272da2deee75426f285b050d9ede248
Brandon Bailey <bwbailey@umich.edu>
2022-03-10 15:02:44 -0500

    Changed values again

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,10 +1,10 @@
 public class AddIntegers {
 
-    // Proof I made a change to the file - Brandon Bailey
+    // Proof I made another change to the file - Brandon Bailey
     public static void main(String[] args) {
 
-        int first = 10;
-        int second = 20;
+        int first = 12;
+        int second = 23;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
